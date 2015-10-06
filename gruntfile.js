30c361ce06af36fe9c854bb1e131cdae0897cd1c
Peak <peak@udtwo.com>
2015-10-06 11:25:10 +0800

    ud2ui 转移至 ud2，重新创建 ud2 结构，添加部分样式与测试用例，完善生产环境和测试环境

---
+++ gruntfile.js
@@ -0,0 +1,111 @@
+﻿/// <binding BeforeBuild='default' ProjectOpened='default' />
+"use strict";
+
+module.exports = function (grunt) {
+	grunt.initConfig({
+		// 读取PACKAGE包
+		pkg: grunt.file.readJSON('package.json'),
+		// 清空文件夹
+		clean: {
+			'dist': {
+				src: ['dist/', 'vendor/']
+			}
+		},
+		// 压缩JS文件
+		uglify: {
+			'script-ud2': {
+				options: {
+					// 是否混淆变量名 true:混淆
+					mangle: true,
+					// 是否删除注释 false:删除全部
+					preserveComments: false,
+					// 头注释
+					banner: '/*! <%= pkg.name %> - v<%= pkg.version %>'
+						+ '\n * (c) <%= grunt.template.today("yyyy") %> Peak(peak@udtwo.com) \n',
+					// 结尾注释
+					footer: '\n/* lasted： <%= grunt.template.today("yyyy-mm-dd hh:MM:ss") %> */'
+				},
+				files: {
+					'dist/js/ud2.js': 'src/js/ud2.js',
+				}
+			},
+			'script-ud2-oldbrowser': {
+				options: {
+					mangle: true,
+					preserveComments: false,
+					banner: '/*! <%= pkg.name %>(oldbrowser) - v<%= pkg.version %>'
+						+ '\n * (c) <%= grunt.template.today("yyyy") %> Peak(peak@udtwo.com) \n',
+					footer: '\n/* lasted： <%= grunt.template.today("yyyy-mm-dd hh:MM:ss") %> */'
+				},
+				files: {
+					'dist/js/ud2.oldbrowser.js': 'src/js/ud2.oldbrowser.js'
+				}
+			}
+		},
+		// 编译LESS
+		less: {
+			'css-ud2': {
+				options: {
+					// 是否压缩 true:是
+					compress: true,
+					// 头注释
+					banner: '/*! <%= pkg.name %>.css - v<%= pkg.version %>'
+						+ '\n * (c) <%= grunt.template.today("yyyy") %> Peak(peak@udtwo.com) \n',
+					footer: '\n/* lasted： <%= grunt.template.today("yyyy-mm-dd hh:MM:ss") %> */'
+				},
+				files: {
+					'dist/css/ud2.css': [
+						'src/less/style/reset.less',
+						'src/less/style/base.less'
+					]
+				}
+			}
+		},
+		// 复制文件
+		copy: {
+			'jquery': {
+				expand: true,
+				cwd: 'bower_components/jQuery/dist/',
+				src: 'jquery.min.js',
+				ext: '.js',
+				dest: 'vendor/js/'
+			},
+			'ico': {
+				expand: true,
+				cwd: 'src/ico/',
+				src: '*',
+				dest: 'dist/ico/'
+			}
+		},
+		// 文件监测
+		watch: {
+			'watch-ud2': {
+				// 监测的文件
+				files: ['src/js/ud2.js'],
+				// 执行任务
+				tasks: ['uglify:script-ud2']
+			},
+			'watch-ud2-oldbrowser': {
+				// 监测的文件
+				files: ['src/js/ud2.oldbrowser.js'],
+				// 执行任务
+				tasks: ['uglify:script-ud2-oldbrowser']
+			},
+			'watch-less-ud2': {
+				// 监测的文件
+				files: ['src/**'],
+				tasks: ['less']
+			}
+		}
+	});
+
+	// 读取NPM任务组件
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-copy');
+	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-less');
+
+	// 建立发布任务
+	grunt.registerTask('default', ['clean', 'copy', 'less', 'uglify']);
+}
